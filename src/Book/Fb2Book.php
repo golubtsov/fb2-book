f46bb583bf15d66708e8d123511c4ffa45a7f0c1
golubtsov <golubtsov.nikita.home@yandex.ru>
2026-03-25 12:49:27 +0700

    Fix Composer metadata and rebuild FB2 XML generation

--- src/Book/Fb2Book.php
+++ src/Book/Fb2Book.php
@@ -2,6 +2,9 @@
 
 namespace Nigo\Fb2Book;
 
+use DOMDocument;
+use DOMElement;
+
 class Fb2Book
 {
     private string $authorFirstName = '';
@@ -14,11 +17,15 @@ class Fb2Book
 
     private string $title = '';
 
-    private string $text = '';
+    /**
+     * @var array<int, array{title: string, text: string}>
+     */
+    private array $sections = [];
 
     public function setTitle(string $title): static
     {
         $this->title = $title;
+
         return $this;
     }
 
@@ -26,112 +33,168 @@ class Fb2Book
     {
         $this->authorFirstName = $firstName;
         $this->authorLastName = $lastName;
+
         return $this;
     }
 
     public function setBookLang(string $lang): static
     {
         $this->lang = $lang;
+
         return $this;
     }
 
     public function setAnnotation(string $annotation): static
     {
         $this->annotation = $annotation;
+
         return $this;
     }
 
     public function addSection(string $text, string $title = ''): static
     {
-        $this->text .= '<section>';
-
-        if (!empty($title)) {
-            $this->text .= "<title><p><strong>$title</strong></p></title>";
-        }
-
-        $this->text .= "<p>$text</p>";
-        $this->text .= '</section>';
+        $this->sections[] = [
+            'title' => $title,
+            'text' => $text,
+        ];
 
         return $this;
     }
 
     public function create(): string
     {
-        $xml = '<?xml version="1.0" encoding="UTF-8"?>';
+        $document = new DOMDocument('1.0', 'UTF-8');
+        $document->formatOutput = false;
 
-        return $xml . '<FictionBook>' . $this->createBookDescription() . $this->createBookBody() . '</FictionBook>';
+        $root = $document->createElement('FictionBook');
+        $root->setAttribute('xmlns', 'http://www.gribuser.ru/xml/fictionbook/2.0');
+        $root->setAttribute('xmlns:l', 'http://www.w3.org/1999/xlink');
+        $document->appendChild($root);
+
+        $root->appendChild($this->createBookDescription($document));
+        $root->appendChild($this->createBookBody($document));
+
+        return $document->saveXML();
     }
 
-    protected function createBookDescription(): string
+    protected function createBookDescription(DOMDocument $document): DOMElement
     {
+        $description = $document->createElement('description');
+        $description->appendChild($this->createTitleInfo($document));
+        $description->appendChild($this->createDocumentInfoAttribute($document));
+        $description->appendChild($this->createPublishInfoAttribute($document));
 
-        $description = $this->createTitleInfo();
-
-        $description .= $this->createDocumentInfoAttribute();
-
-        $description .= $this->createPublishInfoAttribute();
-
-        return "<description>$description</description>";
+        return $description;
     }
 
-    protected function createTitleInfo(): string
+    protected function createTitleInfo(DOMDocument $document): DOMElement
     {
-        $titleInfo = '<title-info>';
-        $titleInfo .= $this->createAuthorAttribute();
-        $titleInfo .= $this->createBookTitleAttribute();
-        $titleInfo .= $this->createAnnotationAttribute();
-        $titleInfo .= $this->createLangAttribute();
-        $titleInfo .= '</title-info>';
+        $titleInfo = $document->createElement('title-info');
+        $titleInfo->appendChild($this->createAuthorAttribute($document));
+        $titleInfo->appendChild($this->createBookTitleAttribute($document));
+        $titleInfo->appendChild($this->createAnnotationAttribute($document));
+        $titleInfo->appendChild($this->createLangAttribute($document));
 
         return $titleInfo;
     }
 
-    protected function createAuthorAttribute(): string
+    protected function createAuthorAttribute(DOMDocument $document): DOMElement
     {
-        $author = '';
+        $author = $document->createElement('author');
 
-        if (!empty($this->authorFirstName)) {
-            $author .= "<first-name>{$this->authorFirstName}</first-name>";
+        if ($this->authorFirstName !== '') {
+            $author->appendChild($document->createElement('first-name', $this->authorFirstName));
         }
 
-        if (!empty($this->authorLastName)) {
-            $author .= "<first-name>{$this->authorLastName}</first-name>";
+        if ($this->authorLastName !== '') {
+            $author->appendChild($document->createElement('last-name', $this->authorLastName));
         }
 
-        return "<author>$author</author>";
+        return $author;
     }
 
-    protected function createBookTitleAttribute(): string
+    protected function createBookTitleAttribute(DOMDocument $document): DOMElement
     {
-        return "<book-title>{$this->title}</book-title>";
+        return $document->createElement('book-title', $this->title);
     }
 
-    protected function createAnnotationAttribute(): string
+    protected function createAnnotationAttribute(DOMDocument $document): DOMElement
     {
-        return "<annotation><p>{$this->annotation}</p></annotation>";
+        $annotation = $document->createElement('annotation');
+
+        if ($this->annotation !== '') {
+            $annotation->appendChild($document->createElement('p', $this->annotation));
+        }
+
+        return $annotation;
     }
 
-    protected function createLangAttribute(): string
+    protected function createLangAttribute(DOMDocument $document): DOMElement
     {
-        return "<lang>{$this->lang}</lang>";
+        return $document->createElement('lang', $this->lang);
     }
 
-    protected function createDocumentInfoAttribute(): string
+    protected function createDocumentInfoAttribute(DOMDocument $document): DOMElement
     {
-        $info = $this->createAuthorAttribute();
+        $documentInfo = $document->createElement('document-info');
+        $documentInfo->appendChild($this->createAuthorAttribute($document));
 
-        return "<document-info>$info</document-info>";
+        return $documentInfo;
     }
 
-    protected function createPublishInfoAttribute(): string
+    protected function createPublishInfoAttribute(DOMDocument $document): DOMElement
     {
-        $publishInfo = $this->title;
+        $publishInfo = $document->createElement('publish-info');
+        $publishInfo->appendChild($document->createElement('book-name', $this->title));
 
-        return "<publish-info><book-name>$publishInfo</book-name></publish-info>";
+        return $publishInfo;
     }
 
-    protected function createBookBody(): string
+    protected function createBookBody(DOMDocument $document): DOMElement
     {
-        return "<body>{$this->text}</body>";
+        $body = $document->createElement('body');
+
+        foreach ($this->sections as $sectionData) {
+            $body->appendChild($this->createSection($document, $sectionData['text'], $sectionData['title']));
+        }
+
+        return $body;
+    }
+
+    protected function createSection(DOMDocument $document, string $text, string $title = ''): DOMElement
+    {
+        $section = $document->createElement('section');
+
+        if ($title !== '') {
+            $titleElement = $document->createElement('title');
+            $paragraph = $document->createElement('p');
+            $strong = $document->createElement('strong', $title);
+            $paragraph->appendChild($strong);
+            $titleElement->appendChild($paragraph);
+            $section->appendChild($titleElement);
+        }
+
+        foreach ($this->splitParagraphs($text) as $paragraphText) {
+            $section->appendChild($document->createElement('p', $paragraphText));
+        }
+
+        return $section;
+    }
+
+    /**
+     * @return list<string>
+     */
+    protected function splitParagraphs(string $text): array
+    {
+        $paragraphs = preg_split('/\R{2,}/u', trim($text)) ?: [];
+
+        if ($paragraphs === []) {
+            return [''];
+        }
+
+        return array_map(
+            static fn (string $paragraph): string => preg_replace('/\R/u', ' ', trim($paragraph)) ?? trim($paragraph),
+            $paragraphs
+        );
     }
 }

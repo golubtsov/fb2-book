c42efdbdc0f5f1b836f1c3129b1fa4956524cdc6
golubtsovns <golubtsovns@gmail.com>
2026-03-25 13:00:58 +0700

    * updated fb2-book.php

--- examples/fb2-book.php
+++ examples/fb2-book.php
@@ -6,14 +6,15 @@ use Nigo\Fb2Book\Fb2Book;
 
 $fb2book = new Fb2Book();
 
-$xmlBook = $fb2book->setTitle('My FB2 book')
-    ->setAuthor('First Name', 'Last Name')
-    ->setAnnotation('My book annotation')
-    ->setBookLang('en')
-    ->addSection('First section for book', 'Chapter one')
-    ->addSection('Second section for book', 'Chapter two')
-    ->create();
+$xmlBook = $fb2book
+  ->setTitle('My FB2 book')
+  ->setAuthor('First Name', 'Last Name')
+  ->setAnnotation('My book annotation')
+  ->setBookLang('en')
+  ->addSection('First section for book', 'Chapter one')
+  ->addSection('Second section for book', 'Chapter two')
+  ->create();
 
 echo $xmlBook;
 
-//file_put_contents('save_path/fb2-book.fb2', $xmlBook);
+file_put_contents('./fb2-book.fb2', $xmlBook);
